204c72319c382f2df04fd0cedacd3c49497318b2
Ryan Dieno <ryan.dieno@gmail.com>
2020-02-01 02:57:23 -0800

    updated movement + added brick pyramid generation

---
+++ Bad Hoomans/Assets/_badhoomans/Scripts/LevelManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject brickPrefab;
+    [SerializeField] private Transform worldPositionAnchor;
+    [SerializeField] private int pyramidHeight;
+    [SerializeField] private float gapSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateTestPyramid(worldPositionAnchor.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void GenerateTestPyramid(Vector3 position)
+    {
+        List<GameObject> bricks = new List<GameObject>();
+
+        SpriteRenderer sr = brickPrefab.GetComponent<SpriteRenderer>();
+
+        //halfWidth = sr.bounds.size.x / 2;
+        //halfHeight = sr.bounds.size.y / 2;
+
+        float brickWidth = sr.bounds.size.x;
+
+
+        //Vector2 currentPosition = worldPositionAnchor.position;
+        Vector2 offset = new Vector2(brickWidth / 2f, brickWidth / 2f);
+
+        int test = pyramidHeight;
+
+        for (int i = 0; i < pyramidHeight; i++)
+        {
+            for (int j = 0; j < test; ++j)
+            {
+
+                Vector2 currentPosition = new Vector2(worldPositionAnchor.position.x + (j * (brickWidth + gapSize)), worldPositionAnchor.position.y + (i * (brickWidth + gapSize)));
+
+                currentPosition.x += (brickWidth / 2f) * i;
+
+                Instantiate(brickPrefab, currentPosition, Quaternion.identity);
+            }
+            test--;
+        }
+
+        //for (int i = pyramidHeight; i >= 0; i--)
+        //{
+        //    for (int j = 0; j <= i; ++j)
+        //    {
+                
+
+        //        Vector2 currentPosition = new Vector2(worldPositionAnchor.position.x + (j * (brickWidth + gapSize)), worldPositionAnchor.position.y + (i * (brickWidth + gapSize)));
+
+        //        currentPosition += offset;
+
+        //        Debug.Log(i + ", " + j + " | " + currentPosition);
+
+               
+
+        //        if(i == pyramidHeight && j == 0)
+        //        {
+        //            Instantiate(brickPrefab, currentPosition, Quaternion.identity);
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ Bad Hoomans/Assets/_badhoomans/Scripts/PlayerController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 12f;
+
+    [SerializeField] private float gravity = 0.1f;
+    [SerializeField] private float drag = 0.5f;
+
+    private float velocityY;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private bool isGrounded = false;
+
+    private float groundY = 0f;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundY = transform.position.y;
+
+        transform.position = new Vector3(transform.position.x, transform.position.x + 25f, transform.position.z);
+
+        rb = GetComponent<Rigidbody2D>();
+
+        //isGrounded = true;
+
+        //if(groundY == transform.position.y)
+        //{
+        //    isGrounded = true;
+        //}
+
+        //isGrounded();
+    }
+
+    //private bool check()
+    //{
+    //    return (groundY == transform.position.y);
+    //}
+
+    // Update is called once per frame
+    void Update() 
+    {
+
+
+        //if(!isGrounded)
+        velocity.y -= gravity;
+
+        //if (velocity.y > 0f)
+        //{
+        //    velocity.y += gravity;
+        //}
+        //else
+        //{
+        //    velocity.y = 0f;
+        //}
+
+        //if (velocity.x > 0f)
+        //{
+            velocity.x *= drag;
+        //}
+        //else
+        //{
+        //    velocity.x = 0f;
+        //}
+
+
+        //Vector3 movement = new Vector3
+        //{
+        //    x = Input.GetAxis("Horizontal"),
+        //    y = 0f,
+        //    z = 0f,
+        //}.normalized;
+
+        ////movement *= speed * Time.deltaTime;
+
+        ////transform.Translate(movement);
+
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            OnJump();
+        }
+
+        velocity.x = Input.GetAxis("Horizontal") * speed;
+
+
+
+
+        //velocityY += gravity * Time.deltaTime;
+
+
+        //transform.position += velocity * Time.deltaTime;
+        rb.MovePosition(transform.position + velocity * Time.deltaTime);
+
+        
+
+
+        if(transform.position.y < groundY)
+        {
+            transform.position = new Vector3(transform.position.x, groundY, transform.position.z);
+            isGrounded = true;
+            velocity.y = 0f;
+        }
+
+
+        //Debug.Log(velocity);
+    }
+
+    void OnJump()
+    {
+        velocity.y = jumpForce;
+        isGrounded = false;
+    }
+}
