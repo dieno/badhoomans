c2becb94d4497e44f37c992629acb855f07213d5
Yasmin <yasminhalwani91@gmail.com>
2020-02-01 21:39:22 -0800

    hp added

--- Bad Hoomans/Assets/_badhoomans/Scripts/GameManager.cs
+++ Bad Hoomans/Assets/_badhoomans/Scripts/GameManager.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System;
 
 public class GameManager : MonoBehaviour
 {
+    // menu scene objects 
     public Button startButton;
     public Button exitButton;
     public Text highScoreText;
+
+    // game scene objects
+    public Text currentScoreText;
+    public Image hpImage;
+
+    // private variables
+    private float currentScore = 0.0f;
+    private double time = -1;
+    private const float hitPoint = 0.5f;
+    private const float maxHp = 10.0f;
+    private float currentHp = maxHp;
+
 
     void StartBtnFunction()
     {
@@ -27,6 +41,25 @@ public class GameManager : MonoBehaviour
         highScoreText.text = "10";
     }
 
+    void takeHit()
+    {
+        if (currentHp > 0)
+        {
+            currentHp -= hitPoint;
+            hpImage.fillAmount -= (hitPoint/maxHp);
+        }
+    }
+
+    void heal()
+    {
+        if(currentHp < maxHp)
+        {
+            currentHp += hitPoint;
+            hpImage.fillAmount += (hitPoint / maxHp);
+        }
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,6 +71,23 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (DateTime.Now.Millisecond != time)
+        {
+            time = DateTime.Now.Millisecond;
+            // fine-tune the timer speed by changing this number
+            currentScore += 0.05f;
+            currentScoreText.text = Mathf.Ceil(currentScore).ToString();
+        }
+
+        // TODO this is only for testing to simulate a hit
+        if (Input.GetKeyDown("z"))
+        {
+            takeHit();
+        }
+        if(Input.GetKeyDown("x"))
+        {
+            heal();
+        }
+
     }
 }
