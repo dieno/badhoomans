980b29a056f56a67a425f1b8be0666e4061aaea0
Ryan Dieno <ryan.dieno@gmail.com>
2020-02-01 19:42:51 -0800

    added floaty movement + sorting order fixes

--- Bad Hoomans/Assets/_badhoomans/Scripts/PlayerController.cs
+++ Bad Hoomans/Assets/_badhoomans/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float yStartOffset = 5f;
 
-    [SerializeField] private Transform handPivot;
+    //[SerializeField] private Transform handPivot;
     [SerializeField] private Transform hand;
 
     [SerializeField] private float speed = 5f;
@@ -16,6 +16,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float gravity = 0.1f;
 
     [SerializeField] private float handRadius = 1f;
+
+    [SerializeField] private float maxHeight = 5f;
+    [SerializeField] private float maxJumpLength = 10f;
 
     private Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
@@ -27,6 +30,11 @@ public class PlayerController : MonoBehaviour
     private bool isHolding = false;
     private GameObject objectUnderHand = null;
 
+    private float currentJumpLength = 0f;
+
+    private bool isJumpCanceled = false;
+    private bool canBeginJump = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -35,24 +43,34 @@ public class PlayerController : MonoBehaviour
 
         // start character above and let him fall in
         transform.position = new Vector3(transform.position.x, transform.position.x + yStartOffset, transform.position.z);
-
     }
 
     void Update() 
     {
         velocity.y -= gravity;
 
+        if (!isGrounded)
+        {
+            currentJumpLength -= Time.deltaTime;
+        }
+
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         currentCursorPosition = new Vector2(worldPoint.x, worldPoint.y);
 
-        if (Input.GetButton("Jump") && isGrounded)
+        if (Input.GetButton("Jump"))
         {
-            OnJumpStart();
+            if(!isJumpCanceled)
+            {
+                OnJumpStart();
+            }
         }
 
-        if(Input.GetButtonUp("Jump") && !isGrounded)
+        if (Input.GetButtonUp("Jump") && !isGrounded)
         {
-            OnJumpEnd();
+            if (!isJumpCanceled)
+            {
+                OnJumpEnd();
+            }
         }
 
         velocity.x = Input.GetAxis("Horizontal") * speed;
@@ -64,30 +82,28 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(transform.position.x, groundY, transform.position.z);
             isGrounded = true;
             velocity.y = 0f;
+            currentJumpLength = 0f;
+            isJumpCanceled = false;
+            canBeginJump = true;
+        }
+
+        if (transform.position.y > maxHeight)
+        {
+            transform.position = new Vector3(transform.position.x, maxHeight, transform.position.z);
+            velocity.y = 0f;
         }
 
         Vector2 direction = currentCursorPosition - new Vector2(transform.position.x, transform.position.y); //direction from Center to Cursor
         Vector2 normalizedDirection = direction.normalized;
 
         hand.position = new Vector2(transform.position.x, transform.position.y) + (normalizedDirection * handRadius);
-
-
+        
         if(Input.GetButton("Fire1"))
         {
             if(isHovering)
             {
                 isHolding = true;
             }
-
-            //if (Input.GetButton("Fire1"))
-            //{
-            //    isHolding = true;
-            //}
-            //else
-            //{
-            //    isHolding = false;
-            //    isHovering = false;
-            //}
         }
         else
         {
@@ -95,7 +111,8 @@ public class PlayerController : MonoBehaviour
             {
                 if (objectUnderHand != null)
                 {
-                    objectUnderHand.layer = LayerMask.NameToLayer("Default");
+                    //objectUnderHand.layer = LayerMask.NameToLayer("Default");
+                    objectUnderHand.GetComponent<SpriteRenderer>().sortingOrder = 1;
                     objectUnderHand = null;
 
                 }
@@ -108,45 +125,48 @@ public class PlayerController : MonoBehaviour
         {
             if(objectUnderHand != null)
             {
-                objectUnderHand.layer = LayerMask.NameToLayer("Ignore Player");
+                //objectUnderHand.layer = LayerMask.NameToLayer("Ignore Player");
+                objectUnderHand.GetComponent<SpriteRenderer>().sortingOrder = 3;
                 objectUnderHand.GetComponent<Rigidbody2D>().MovePosition(hand.position);
                 objectUnderHand.transform.rotation = Quaternion.identity;
                 objectUnderHand.GetComponent<Rigidbody2D>().angularVelocity = 0f;
-                //objectUnderHand.transform.position = hand.position;
-                //objectUnderHand = null;
             }
-
-            //if (Input.GetButtonUp("Fire1"))
-            //{
-            //    isHolding = false;
-            //    //objectUnderHand = null;
-            //    //Debug.Log("isholding");
-            //}
         }
+
+        if(!isGrounded && currentJumpLength <= 0f)
+        {
+            isJumpCanceled = true;
+        }
+
+        //Debug.Log(currentJumpLength);
     }
 
     private void OnJumpStart()
     {
         velocity.y = jumpForce;
         isGrounded = false;
+
+        if(canBeginJump)
+        {
+            currentJumpLength = maxJumpLength;
+            canBeginJump = false;
+        }
+       
     }
 
     private void OnJumpEnd()
     {
-        if (velocity.y > 6.0f)
-            velocity.y = 6.0f;
+        velocity.y = 0f;
+
+        //if (velocity.y > 6.0f)
+        //    velocity.y = 6.0f;
     }
 
     private void OnDrawGizmos()
     {
-        // Draw a yellow sphere at the transform's position
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(currentCursorPosition, 0.25f);
-
-        //Gizmos.color = Color.magenta;
-       // Gizmos.DrawLine(handPivot.position, handPivot.position + (handPivot.forward * 100f));
     }
-
 
     public void HandleBrickHandsCollision(GameObject brick)
     {
@@ -154,19 +174,8 @@ public class PlayerController : MonoBehaviour
         {
             return;
         }
-
-
+        
         objectUnderHand = brick;
         isHovering = true;
-    }
-
-    public void BeginBrickHandsTrigger()
-    {
-
-    }    
-
-    public void EndBrickHandsTrigger()
-    {
-
     }
 }
